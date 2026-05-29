04a09940e4e45f716e4e4f728e19a26644e7b262
Joash98413 <kibetjoash90@gmail.com>
2026-05-29 11:41:27 +0300

    remove backend folder

--- frontend/src/pages/AddRoom.jsx
+++ frontend/src/pages/AddRoom.jsx
@@ -1,3 +1,13 @@
+import { useState } from "react"
+
+const AddRoom = () => {
+    const [formData, setFoemData] = useState({
+        name: "",
+        price: "",
+        description: ""
+    })
+}
+
 const handleSubmit = async (e) => {
     e.preventDefault()
 
@@ -10,5 +20,21 @@ const handleSubmit = async (e) => {
     const data = await res.json()
     console.log(data)
 }
+
+return(
+    <div>
+        <h2>AddRoom</h2>
+        <form OnSubmit={handleSubmit}>
+            <input 
+            type="text" 
+            placeholder="Room name"
+            value={formData.name}
+            onChange={(e) => setFormData({...formData, name: e.target.value})}
+            />
+            <button type="submit">Add Room</button>
+        </form>
+    </div>
+)
+
 
 export default AddRoom
